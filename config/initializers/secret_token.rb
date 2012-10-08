811ff4ae97eb1ada59cb341ccc6645ed229d8a2f
Tatiana E Chibisova <tataxfn@gmail.com>
2012-10-07 17:27:39 -0700

    Initializing Rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Notyourmom::Application.config.secret_token = '94c9eabbac3bf85600ed44524c5b4a6838f484eafa26d2c6a5c4c8d52be825c221d7aa39def2297801725354eaf1cb18b73a50e0318acde83cf63fe8dd8ec77c'
