acb113e62bb2ad518d7289efdf6ff9752f9a78ec
Devang Sampat <devangs27@gmail.com>
2012-11-05 16:54:03 -0800

    Adding a full name setter/getter for the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,16 @@ class User < ActiveRecord::Base
   has_many :expenses
   has_many :debts
 
+  def full_name
+    [firstName, lastName].join(' ')
+  end
+
+  def full_name=(name)
+    split = name.split(' ', 2)
+    self.firstName = split.first
+    self.lastName = split.last
+  end
+
   def join_household=(house_id)
     self.household_id = house_id
   end
