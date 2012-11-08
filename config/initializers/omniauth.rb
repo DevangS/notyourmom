0dc5ba4a76ac31da31ceaaa5ca246dd2a7f2e096
Theresa Calderon <tcaldero@ucsd.edu>
2012-11-08 08:50:24 -0800

    Changing APP_ID and APP_SECRET

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   # The following is for facebook
-  provider :facebook, "107777596051571", "822241b5911fd71a1ee9ab789fe671c3", 
+  provider :facebook, "455996127785930", "cc7631300dc0f2d667cd50e83b4f8bd7", 
   { :scope => 'email, read_stream, read_friendlists, 
   	friends_likes, friends_status, offline_access' }
  
