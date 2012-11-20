8b37171a97e72e274f9bd5a4698ed57e84a4188c
Tatiana Chibisova <tataxfn@gmail.com>
2012-11-19 20:17:36 -0800

    Took logic out of the home page

--- app/models/debt.rb
+++ app/models/debt.rb
@@ -5,4 +5,8 @@ class Debt < ActiveRecord::Base
   validates_presence_of :user, :expense
 
   validates_numericality_of :percentage_owed, :greater_than => 0, :less_than_or_equal_to => 100
+
+  def get_share
+    self.percentage_owed * self.expense.price/100
+  end
 end
