2ecc8ac8a8eff77e90eecadbb656efaa2af6dccf
Theresa Calderon <tcaldero@ucsd.edu>
2012-11-26 19:34:48 -0800

    Send Reminders without cron job yet and fix homepage

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -1,3 +1,4 @@
+require 'date'
 class RemindersController < ApplicationController
   before_filter :authenticate_user!
 
@@ -31,6 +32,7 @@ class RemindersController < ApplicationController
   # POST /reminders
   # POST /reminders.json
   def create
+    #params[:reminder][:date] = DateTime.parse(params[:reminder][:date])
     @reminder = Reminder.new(params[:reminder])
 
     respond_to do |format|
