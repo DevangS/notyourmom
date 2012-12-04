d689765f6a48675ffa33ddc9ea8b7d9e56fa462e
Jenny Huang <jenijenny@gmail.com>
2012-12-04 15:03:06 -0800

    ....

--- db/schema.rb
+++ db/schema.rb
@@ -101,27 +101,20 @@ ActiveRecord::Schema.define(:version => 20121204062817) do
     t.string   "password"
     t.string   "fb_key"
     t.integer  "household_id"
-    t.string   "encrypted_password",                   :default => ""
+    t.string   "encrypted_password",     :default => "", :null => false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",                        :default => 0
+    t.integer  "sign_in_count",          :default => 0
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.string   "invitation_token",       :limit => 60
-    t.datetime "invitation_sent_at"
-    t.datetime "invitation_accepted_at"
-    t.integer  "invited_by_id"
-    t.string   "invited_by_type"
     t.integer  "invitation_id"
     t.integer  "invitation_limit"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-  add_index "users", ["invitation_token"], :name => "index_users_on_invitation_token"
-  add_index "users", ["invited_by_id"], :name => "index_users_on_invited_by_id"
   add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
