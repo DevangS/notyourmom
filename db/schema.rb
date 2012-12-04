04998dead470588b5d18d95e6e9e3bce96fe2377
Theresa Calderon <tcaldero@ucsd.edu>
2012-12-04 00:34:20 -0800

    Fix sign in top bar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121124050441) do
+ActiveRecord::Schema.define(:version => 20121204043909) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -115,6 +115,7 @@ ActiveRecord::Schema.define(:version => 20121124050441) do
     t.string   "last_sign_in_ip"
     t.integer  "invitation_id"
     t.integer  "invitation_limit"
+    t.string   "image"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
