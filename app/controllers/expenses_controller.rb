e817dee4d9af210b5868bfd7cbda66f04f8b2761
Jenny Huang <jenijenny@gmail.com>
2012-12-04 15:02:08 -0800

    Pwetty Stuff.

--- app/controllers/expenses_controller.rb
+++ app/controllers/expenses_controller.rb
@@ -6,7 +6,7 @@ class ExpensesController < ApplicationController
   # GET /expenses.json
   def index
     time = Time.new
-    @the_date = time.month.to_s + "/" + time.day.to_s + "/" + time.year.to_s
+    @the_date = time.year.to_s + '-' +time.month.to_s + "-" + time.day.to_s
     @expenses = Expense.where(:resolved => false, :household_id => current_user.household_id)
     @expenses_done = Expense.where(:resolved => true, :household_id => current_user.household_id)
     @expenses.each do |e|
