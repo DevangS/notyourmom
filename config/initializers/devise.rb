a9db5cc8f2dde2f74f7933bb99840aee43968fd1
Scott Ngo <swngo@VSOP-DEV.(none)>
2012-12-05 19:39:07 -0800

    Changed configs

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -229,4 +229,5 @@ Devise.setup do |config|
   # When using omniauth, Devise cannot automatically set Omniauth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = "/my_engine/users/auth"
+  # config.action_mailer.default_url_option = { :host => 'notyourmom.heroku.com' }
 end
